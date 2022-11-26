9ad3748f2ad0eb49386e5d02475c921438becccd
Yu Shi <58924396+missyushi@users.noreply.github.com>
2022-11-26 16:47:36 -0500

    Initial commit

---
+++ tools/mk_tex_table.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+for arg in $(ls *tex | grep -v pdf_)
+do
+  echo "\documentclass{article}
+	\usepackage[utf8]{inputenc}
+	\usepackage{graphicx}
+	\usepackage[landscape,margin=0.5in]{geometry}
+        \usepackage{hyperref}
+	\usepackage{xcolor}
+	\usepackage{subfig}
+	\begin{document}
+	" > pdf_$arg
+	cat $arg >> pdf_$arg
+	echo "\end{document}" >> pdf_$arg
+	pdflatex pdf_$arg
+done
