02bb784037d6c204e3225bf028c7c70478adc200
Yu Shi <58924396+missyushi@users.noreply.github.com>
2023-01-15 18:28:45 -0500

    add two .sh files

---
+++ modified_codes/Data/ReplicationFolder_CJE_Covid/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd Data/ReplicationFolder_CJE_Covid
+stata-mp -b do Main.do
---
+++ run-all.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# run the project specific run.sh
+
+cd modified_codes/Data/ReplicationFolder_CJE_Covid
+chmod a+rx run.sh
+./run.sh
+
